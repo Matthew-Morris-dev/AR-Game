0ade252a4e0a5de1b2e71a46e230a79a68eb47a1
Matthew Morris <43638221+dffiaNt@users.noreply.github.com>
2019-03-28 22:41:06 +0200

    Unity Source
    AR Camera in and working

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera firstPersonCamera;
+    private bool _Quitting = false;
+    public DetectedPlane detectedPlane;
+    private bool planeSet = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuitOnConnectionErrors();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //session must be tracking in order access the frame
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            return;
+        }
+        //if we are tracking check if screen is touched
+        if (!planeSet)
+        {
+            ProcessTouches();
+        }
+    }
+
+    //This will detect if user touches the screen.
+    //if so then cast ray from camera to the touched position and check if it hits a ARCore detected plane
+    private void ProcessTouches()
+    {
+        Touch touch;
+        if ((Input.touchCount != 1) || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinBounds | TrackableHitFlags.PlaneWithinPolygon;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            SetSelectedPlane(hit.Trackable as DetectedPlane);
+        }
+    }
+
+    //Used to test if raycast is working as intended
+    private void SetSelectedPlane(DetectedPlane selectedPlane)
+    {
+        Debug.Log("selected plane at " + selectedPlane.CenterPose.position);
+        detectedPlane = selectedPlane;
+    }
+
+    //checks the ARCore session and that ARCore is working in our application
+    private void QuitOnConnectionErrors()
+    {
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            _Quitting = true;
+            Invoke("_Quit()", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+            _Quitting = true;
+            Invoke("_Quit()", 0.5f);
+        }
+    }
+
+    //close application
+    private void _Quit()
+    {
+        Application.Quit();
+    }
+    // Taken Directly from ARCore HELLOAR example (I understand what this does but not how it works !! need to learn)
+    /// <summary>
+    /// Show an Android toast message.
+    /// </summary>
+    /// <param name="message">Message string to show in the toast.</param>
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+                    message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using GoogleARCore;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Minions;
+    public GameObject playerCharacter;
+    public Text currentMinionText;
+    private bool _playerCharacterAlive = false;
+    private GameManager _gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gm = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //session must be tracking in order access the frame
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            return;
+        }
+        //if we are tracking check if screen is touched
+        ProcessTouches();
+    }
+
+    //This will detect if user touches the screen.
+    //if so then cast ray from camera to the touched position and check if it hits a ARCore detected plane
+    private void ProcessTouches()
+    {
+        Touch touch;
+        if ((Input.touchCount != 1) || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+        if (!EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        {
+            TrackableHit hit;
+            TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinBounds | TrackableHitFlags.PlaneWithinPolygon;
+
+            if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit) && _playerCharacterAlive == false)
+            {
+                //anchor hopefully keeps the players character model in a good place.
+                Anchor anchor = _gm.detectedPlane.CreateAnchor(new Pose(hit.Pose.position, Quaternion.identity));
+                Instantiate(playerCharacter, hit.Pose.position, Quaternion.identity, anchor.transform);
+                _playerCharacterAlive = true;
+            }
+        }
+    }
+    /* might use later
+    private void SpawnFoodInstance()
+    {
+        insideFoodSpawnerText.enabled = true;
+        GameObject foodItem = foodModels[Random.Range(0, foodModels.Length)];
+
+        List<Vector3> vertices = new List<Vector3>();
+        _sc.detectedPlane.GetBoundaryPolygon(vertices);
+        Vector3 pt = vertices[Random.Range(0, vertices.Count)];
+        float dist = Random.Range(0.05f, 1f);
+        Vector3 position = Vector3.Lerp(pt, _sc.detectedPlane.CenterPose.position, dist);
+        //move the object above the plane
+        position.y += .05f;
+
+        Anchor anchor = _sc.detectedPlane.CreateAnchor(new Pose(position, Quaternion.identity));
+
+        foodObject = Instantiate(foodItem, position, Quaternion.identity, anchor.transform);
+
+        //Set the tag
+        foodObject.tag = "food";
+
+        foodObject.transform.localScale = new Vector3(.25f, .25f, .25f);
+        foodObject.transform.SetParent(anchor.transform);
+        foodAge = 0;
+
+        foodObject.AddComponent<FoodMotion>();
+        _sc.foodTagText.text = "food tag is: " + foodObject.tag;
+        Debug.Log("we spawned: " + foodObject.name);
+    }
+    */
+
+    public void SetPlayerCharacterToCube()
+    {
+        playerCharacter = Minions[0];
+        currentMinionText.text = "Current Minion:" + playerCharacter.name;
+    }
+
+    public void SetPlayerCharacterToSphere()
+    {
+        playerCharacter = Minions[1];
+        currentMinionText.text = "Current Minion: " + playerCharacter.name;
+    }
+}
